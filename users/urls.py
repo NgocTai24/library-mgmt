96910f6148685d7afafd93643838a363c1527661
Ngọc Tài <165184546+NgocTai24@users.noreply.github.com>
2025-09-26 16:47:06 +0700

    feat:  authenticate

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.user_list, name='user_list'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('<int:user_id>/', views.user_detail, name='user_detail'),  
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,43 @@
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.decorators import user_passes_test
+from .forms import RegisterForm
 
+@user_passes_test(lambda u: u.is_staff)
 def user_list(request):
     users = User.objects.all()
     return render(request, 'users/user_list.html', {'users': users})
+
+def register(request):
+    if request.method == 'POST':
+       form = RegisterForm(request.POST)
+       if form.is_valid():
+           user = form.save(commit=False)
+           user.set_password(form.cleaned_data['password'])
+           user.save()
+           return redirect('user_list')
+    else:
+        form = RegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('user_list')
+        else:
+            error = "Sai tài khoản hoặc mật khẩu"
+            return render(request, 'users/login.html', {'error': error})
+    return render(request, 'users/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def user_detail(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    return render(request, 'users/user_detail.html', {'user': user})
